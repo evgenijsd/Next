74eb123928db81508447c2564b1e03eb9ecab26a
denis.vedernikov <denis.vedernikov@headworks.com.ua>
2022-01-20 18:54:38 +0200

    Create configurations project(DEV, STAGE, RELEASE)

---
+++ Next2/Next2/Next2/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Application
+{
+    public static class Constants
+    {
+#if RELEASE
+        public const string BASE_URL = "dfdfdffd";
+#elif STAGE
+            public const string BASE_URL = "dfdfdffd";
+#elif DEV
+            public const string BASE_URL = "dfdfdffd";
+#else
+        public const string BASE_URL = "dfdfdffd";
+#endif
+    }
+}
