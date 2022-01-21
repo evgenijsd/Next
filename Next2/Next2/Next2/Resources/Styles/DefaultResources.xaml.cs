32ad1f2bd3d75db0ac024c750e56c00e9c46c3f2
oleksii_orel <oleksii.orel@headworks.com.ua>
2022-01-21 19:35:55 +0200

    Implemented style Inserted Barlow Fonts

---
+++ Next2/Next2/Next2/Resources/Styles/DefaultResources.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Next2.Resources.Styles
+{
+    public partial class DefaultResources : ResourceDictionary
+    {
+        public DefaultResources()
+        {
+            InitializeComponent();
+        }
+    }
+}
