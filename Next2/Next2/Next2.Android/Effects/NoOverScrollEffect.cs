4526ad1dac787ce0bfb8278fa57cbbbe6a8eac20
DESKTOP-6M55UET\Intern_01 <andreytancyura@gmail.com>
2022-01-26 20:14:57 +0200

    Added NoOverScrollEffect, completed layout for android

---
+++ Next2/Next2/Next2.Android/Effects/NoOverScrollEffect.cs
@@ -0,0 +1,20 @@
+﻿using Next2.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportEffect(typeof(NoOverScrollEffect), nameof(NoOverScrollEffect))]
+
+namespace Next2.Droid.Effects
+{
+    public class NoOverScrollEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            Control.OverScrollMode = Android.Views.OverScrollMode.Never;
+        }
+
+        protected override void OnDetached()
+        {
+        }
+    }
+}
