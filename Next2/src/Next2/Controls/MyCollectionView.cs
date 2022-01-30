a95283af913ffaeabe18ab65a028888bb0c11d00
andreytantsyura <andreytancyura@gmail.com>
2022-01-30 13:39:55 +0300

    Created a renderer to customize the CollectionView scrollbar (width and color)

---
+++ Next2/src/Next2/Controls/MyCollectionView.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Next2.Controls
+{
+    public class MyCollectionView : CollectionView
+    {
+        public Color ScrollColor { get; set; }
+
+        public int ScrollBarWidth { get; set; }
+
+        public int ScrollValue_1 { get; set; }
+        public int ScrollValue_2 { get; set; }
+        public int ScrollValue_4 { get; set; }
+        public int ScrollValue_5 { get; set; }
+    }
+}
