a0386d73bdd53eefd3c8bd3bcbc70c10bc96b5ed
yevhen demchenko <yevhen.demchenko@headworks.com.ua>
2022-02-01 21:25:54 +0200

    Edit iOS

--- Next2/src/Next2/Views/BaseContentPage.cs
+++ Next2/src/Next2/Views/BaseContentPage.cs
@@ -1,4 +1,6 @@
 ﻿using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace Next2.Views
 {
@@ -6,7 +8,8 @@ namespace Next2.Views
     {
         public BaseContentPage()
         {
-            NavigationPage.SetHasNavigationBar(this, false);
+            On<iOS>().SetUseSafeArea(true);
+            Xamarin.Forms.NavigationPage.SetHasNavigationBar(this, false);
         }
     }
 }
