51fb85a33e63e1abb9cf1a9604ac663655d0fa30
DESKTOP-6M55UET\Intern_01 <andreytancyura@gmail.com>
2022-02-08 21:49:21 +0200

    NEX-18: - added order page styles; - the order page has been partially finalized; - added disappearing of the Tax button after clicking; - the Tax icon is added; - the CustomStepper is partially completed; - duplicates of links to assets are removed.

---
+++ Next2/src/Next2/Controls/CustomStepper.xaml.cs
@@ -0,0 +1,193 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Next2.Controls
+{
+    public partial class CustomStepper : StackLayout
+    {
+        public CustomStepper()
+        {
+            InitializeComponent();
+        }
+
+        #region -- Public properties --
+
+        public static readonly BindableProperty DecrementIconSourceProperty = BindableProperty.Create(
+          propertyName: nameof(DecrementIconSource),
+          returnType: typeof(string),
+          declaringType: typeof(CustomStepper),
+          defaultBindingMode: BindingMode.TwoWay);
+
+        public string DecrementIconSource
+        {
+            get => (string)GetValue(DecrementIconSourceProperty);
+            set => SetValue(DecrementIconSourceProperty, value);
+        }
+
+        public static readonly BindableProperty IncrementIconSourceProperty = BindableProperty.Create(
+            propertyName: nameof(IncrementIconSource),
+            returnType: typeof(string),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public string IncrementIconSource
+        {
+            get => (string)GetValue(IncrementIconSourceProperty);
+            set => SetValue(IncrementIconSourceProperty, value);
+        }
+
+        public static readonly BindableProperty IconSizesProperty = BindableProperty.Create(
+            propertyName: nameof(IconSizes),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int IconSizes
+        {
+            get => (int)GetValue(IconSizesProperty);
+            set => SetValue(IconSizesProperty, value);
+        }
+
+        public static readonly BindableProperty TextColorProperty = BindableProperty.Create(
+            propertyName: nameof(TextColor),
+            returnType: typeof(Color),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public Color TextColor
+        {
+            get => (Color)GetValue(TextColorProperty);
+            set => SetValue(TextColorProperty, value);
+        }
+
+        public static readonly BindableProperty FontFamilyProperty = BindableProperty.Create(
+            propertyName: nameof(FontFamily),
+            returnType: typeof(string),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public string FontFamily
+        {
+            get => (string)GetValue(FontFamilyProperty);
+            set => SetValue(FontFamilyProperty, value);
+        }
+
+        public static readonly BindableProperty FontSizeProperty = BindableProperty.Create(
+            propertyName: nameof(FontSize),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int FontSize
+        {
+            get => (int)GetValue(FontSizeProperty);
+            set => SetValue(FontSizeProperty, value);
+        }
+
+        public static readonly BindableProperty ValueFormatProperty = BindableProperty.Create(
+            propertyName: nameof(ValueFormat),
+            returnType: typeof(string),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public string ValueFormat
+        {
+            get => (string)GetValue(ValueFormatProperty);
+            set => SetValue(ValueFormatProperty, value);
+        }
+
+        public static readonly BindableProperty TextProperty = BindableProperty.Create(
+            propertyName: nameof(Text),
+            returnType: typeof(string),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create(
+            propertyName: nameof(Value),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int Value
+        {
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public static readonly BindableProperty IncrementValueProperty = BindableProperty.Create(
+            propertyName: nameof(IncrementValue),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int IncrementValue
+        {
+            get => (int)GetValue(IncrementValueProperty);
+            set => SetValue(IncrementValueProperty, value);
+        }
+
+        public static readonly BindableProperty MinValueProperty = BindableProperty.Create(
+            propertyName: nameof(MinValue),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int MinValue
+        {
+            get => (int)GetValue(MinValueProperty);
+            set => SetValue(MinValueProperty, value);
+        }
+
+        public static readonly BindableProperty MaxValueProperty = BindableProperty.Create(
+            propertyName: nameof(MaxValue),
+            returnType: typeof(int),
+            declaringType: typeof(CustomStepper),
+            defaultBindingMode: BindingMode.TwoWay);
+
+        public int MaxValue
+        {
+            get => (int)GetValue(MaxValueProperty);
+            set => SetValue(MaxValueProperty, value);
+        }
+
+        private ICommand _decrementCommand;
+        public ICommand DecrementCommand => _decrementCommand ??= new AsyncCommand(OnDecrementCommandAsync);
+
+        private ICommand _incrementCommand;
+        public ICommand IncrementCommand => _incrementCommand ??= new AsyncCommand(OnIncrementCommandAsync);
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private Task OnDecrementCommandAsync()
+        {
+            if (Value - IncrementValue >= MinValue)
+            {
+                Value -= IncrementValue;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private Task OnIncrementCommandAsync()
+        {
+            if (Value + IncrementValue <= MaxValue)
+            {
+                Value += IncrementValue;
+            }
+
+            return Task.CompletedTask;
+        }
+
+        #endregion
+    }
+}
---
+++ Next2/src/Next2/Converters/ValueToFormattedStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Next2.Converters
+{
+    public class ValueToFormattedStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string re = string.Format(parameter as string, value);
+                return re;
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
