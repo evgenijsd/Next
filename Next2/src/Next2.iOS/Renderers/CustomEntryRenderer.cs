4897ba2904baa1a4edbf90c89211e22c888845a6
oleksii_orel <oleksii.orel@headworks.com.ua>
2022-02-21 12:02:58 +0200

    Implemented Done button on a numerik keyboard for Mobile on IOs platform

--- Next2/src/Next2.iOS/Renderers/CustomEntryRenderer.cs
+++ Next2/src/Next2.iOS/Renderers/CustomEntryRenderer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using Foundation;
 using Next2.Controls;
 using Next2.iOS.Renderers;
@@ -20,7 +21,16 @@ namespace Next2.iOS.Renderers
             {
                 Control.BorderStyle = UIKit.UITextBorderStyle.None;
             }
+
+            if (this.Element == null)
+                return;
+
+            // Check only for Numeric keyboard
+            if (this.Element.Keyboard == Keyboard.Numeric)
+                this.AddDoneButton();
         }
+
+        #region -- Overrides --
 
         public override bool CanPerform(Selector action, NSObject withSender)
         {
@@ -28,5 +38,30 @@ namespace Next2.iOS.Renderers
 
             return base.CanPerform(action, withSender);
         }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private void AddDoneButton()
+        {
+            var toolbar = new UIToolbar(new RectangleF(0.0f, 0.0f, 50.0f, 44.0f));
+
+            var doneButton = new UIBarButtonItem(UIBarButtonSystemItem.Done, (sender, e) =>
+            {
+                this.Control.ResignFirstResponder();
+                var baseEntry = this.Element.GetType();
+                ((IEntryController)Element).SendCompleted();
+            });
+
+            toolbar.Items = new UIBarButtonItem[] {
+                new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace),
+                doneButton
+            };
+            this.Control.InputAccessoryView = toolbar;
+        }
+
+        #endregion
+
     }
 } 
