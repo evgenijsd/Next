65162aaad67ef46d116a04ee05fe15e2e92f4c78
Yevhen Demchenko <yevhen.demchenko@headworks.com.ua>
2022-03-08 16:54:03 +0200

    NEX-35:  - create model

---
+++ Next2/src/Next2/ENums/EDiscountType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Next2.ENums
+{
+    public enum EDiscountType
+    {
+        Value,
+        Percent,
+    }
+}
---
+++ Next2/src/Next2/Models/DiscountModel.cs
@@ -0,0 +1,14 @@
+﻿using Next2.ENums;
+using Next2.Interfaces;
+
+namespace Next2.Models
+{
+    public class DiscountModel : IBaseModel
+    {
+        public int Id { get; set; }
+
+        public double Value { get; set; }
+
+        public EDiscountType Type { get; set; }
+    }
+}
