cd62ebe288052136a27f41343e04fa9dcf57a752
oleksii_orel <oleksii.orel@headworks.com.ua>
2022-03-14 19:15:30 +0200

    safe progress

--- Next2/src/Next2/ViewModels/Mobile/EditPageViewModel.cs
+++ Next2/src/Next2/ViewModels/Mobile/EditPageViewModel.cs
@@ -3,21 +3,35 @@ using System.Windows.Input;
 using System.Threading.Tasks;
 using Xamarin.CommunityToolkit.ObjectModel;
 using Next2.Models;
+using Next2.Services.Order;
+using System.Linq;
 
 namespace Next2.ViewModels.Mobile
 {
     public class EditPageViewModel : BaseViewModel
     {
+        private readonly IOrderService _orderService;
+        private int _idxSeat;
+        private int _idxSet;
+        private SetBindableModel _set;
         private SetBindableModel _selectedDish;
-
-        public EditPageViewModel(INavigationService navigationService)
+        public EditPageViewModel(
+            INavigationService navigationService,
+            IOrderService orderService)
           : base(navigationService)
         {
+            _orderService = orderService;
+
+            var seat = _orderService.CurrentOrder.Seats.FirstOrDefault(row => row.SelectedItem != null);
+
+            _idxSeat = _orderService.CurrentOrder.Seats.IndexOf(seat);
+            _set = _orderService.CurrentOrder.Seats[_idxSeat].SelectedItem;
+            _idxSet = seat.Sets.IndexOf(_set);
         }
 
         #region -- Public properties --
 
-        public SetBindableModel SelectedDish { get; set; }
+        public SetBindableModel SelectedDish { get; set; } = null;
 
         private ICommand _openModifyCommand;
         public ICommand OpenModifyCommand => _openModifyCommand ??= new AsyncCommand(OnOpenModifyCommandAsync);
@@ -34,7 +48,14 @@ namespace Next2.ViewModels.Mobile
 
         public override async void OnNavigatedTo(INavigationParameters parameters)
         {
-            SelectedDish = parameters.TryGetValue("SelectedDish", out _selectedDish) ? _selectedDish : null;
+            if(parameters.TryGetValue("SelectedDish", out _selectedDish))
+            {
+                SelectedDish = _selectedDish;
+            }
+            else
+            {
+                SelectedDish = _orderService.CurrentOrder.Seats[_idxSeat].Sets[_idxSet];
+            }
         }
 
         #endregion
@@ -46,9 +67,9 @@ namespace Next2.ViewModels.Mobile
             return Task.CompletedTask;
         }
 
-        private Task OnOpenModifyCommandAsync()
+        private async Task OnOpenModifyCommandAsync()
         {
-            return Task.CompletedTask;
+            await _navigationService.NavigateAsync(nameof(Views.Mobile.ModificationsPage));
         }
 
         private Task OnOpenRemoveCommandAsync()
