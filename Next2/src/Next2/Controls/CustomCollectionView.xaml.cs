90026857c7bc01010096776e6ab544d52c2eb5b8
Yevhen Demchenko <yevhen.demchenko@headworks.com.ua>
2022-04-06 12:49:57 +0300

    NEX-35: - add custom collection

--- Next2/src/Next2/Controls/CustomCollectionView.xaml.cs
+++ Next2/src/Next2/Controls/CustomCollectionView.xaml.cs
@@ -11,13 +11,17 @@ using Xamarin.Forms.Xaml;
 
 namespace Next2.Controls
 {
-    public partial class CustomCollectionView : StackLayout
+    public partial class CustomCollectionView : Grid
     {
         private readonly double _heightBonus = App.IsTablet ? Constants.LayoutBonuses.ROW_TABLET_BONUS : Constants.LayoutBonuses.ROW_MOBILE_BONUS;
 
         public CustomCollectionView()
         {
             InitializeComponent();
+
+            HeightBonusAll = _heightBonus * 2;
+
+            HeightAll = HeightBonusAll + 30;
         }
 
         #region -- Public properties --
@@ -44,90 +48,9 @@ namespace Next2.Controls
             set => SetValue(SelectedBonusProperty, value);
         }
 
-        public static readonly BindableProperty HeightBonusProperty = BindableProperty.Create(
-            propertyName: nameof(HeightBonus),
-            returnType: typeof(double),
-            defaultValue: 50.0d,
-            declaringType: typeof(CustomCollectionView));
+        public double HeightBonusAll { get; set; }
 
-        public double HeightBonus
-        {
-            get => (double)GetValue(HeightBonusProperty);
-            set => SetValue(HeightBonusProperty, value);
-        }
-
-        public static readonly BindableProperty TextProperty = BindableProperty.Create(
-            propertyName: nameof(Text),
-            returnType: typeof(string),
-            declaringType: typeof(CustomCollectionView));
-
-        public string Text
-        {
-            get => (string)GetValue(TextProperty);
-            set => SetValue(TextProperty, value);
-        }
-
-        public static readonly BindableProperty TapSelectCommandProperty = BindableProperty.Create(
-            propertyName: nameof(TapSelectCommand),
-            returnType: typeof(ICommand),
-            declaringType: typeof(CustomCollectionView),
-            defaultBindingMode: BindingMode.TwoWay);
-
-        public ICommand TapSelectCommand
-        {
-            get => (ICommand)GetValue(TapSelectCommandProperty);
-            set => SetValue(TapSelectCommandProperty, value);
-        }
-
-        public static readonly BindableProperty TapSelectCommandParameterProperty = BindableProperty.Create(
-            propertyName: nameof(TapSelectCommandParameter),
-            returnType: typeof(BonusBindableModel),
-            declaringType: typeof(CustomCollectionView),
-            defaultBindingMode: BindingMode.TwoWay);
-
-        public object TapSelectCommandParameter
-        {
-            get => (BonusBindableModel)GetValue(TapSelectCommandParameterProperty);
-            set => SetValue(TapSelectCommandParameterProperty, value);
-        }
-
-        public static readonly BindableProperty ImagePathProperty = BindableProperty.Create(
-            propertyName: nameof(ImagePath),
-            returnType: typeof(string),
-            defaultValue: "ic_check_box_unchecked_white",
-            declaringType: typeof(CustomCollectionView));
-
-        public string ImagePath
-        {
-            get => (string)GetValue(ImagePathProperty);
-            set => SetValue(ImagePathProperty, value);
-        }
-
-        public static readonly BindableProperty BorderBonusColorProperty = BindableProperty.Create(
-            propertyName: nameof(BorderBonusColor),
-            returnType: typeof(Color),
-            declaringType: typeof(CustomCollectionView),
-            defaultBindingMode: BindingMode.TwoWay);
-
-        public Color BorderBonusColor
-        {
-            get => (Color)GetValue(BorderBonusColorProperty);
-            set => SetValue(BorderBonusColorProperty, value);
-        }
-
-        public static readonly BindableProperty BackColorProperty = BindableProperty.Create(
-            propertyName: nameof(BackColor),
-            returnType: typeof(Color),
-            declaringType: typeof(CustomCollectionView),
-            defaultBindingMode: BindingMode.TwoWay);
-
-        public Color BackColor
-        {
-            get => (Color)GetValue(BackColorProperty);
-            set => SetValue(BackColorProperty, value);
-        }
-
-        public double HeightBonusLine = App.IsTablet ? Constants.LayoutBonuses.ROW_TABLET_BONUS : Constants.LayoutBonuses.ROW_MOBILE_BONUS;
+        public double HeightAll { get; set; }
 
         #endregion
     }
