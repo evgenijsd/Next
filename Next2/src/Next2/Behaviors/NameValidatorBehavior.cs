713dae50bac2a3f82baf532a8f0d0a2349675a48
DESKTOP-6M55UET\Intern_01 <andreytancyura@gmail.com>
2022-04-18 21:30:06 +0300

    NEX-167: - added correction of the client's name register; - added control of the correctness of the client's name; - changed the keyboard for the email input field; - fixed indentation inside the client name.

---
+++ Next2/src/Next2/Behaviors/NameValidatorBehavior.cs
@@ -0,0 +1,65 @@
+﻿using Next2.Controls;
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Next2.Behaviors
+{
+    public class NameValidatorBehavior : Behavior<CustomEntry>
+    {
+        #region -- Overrides --
+
+        protected override void OnAttachedTo(CustomEntry bindable)
+        {
+            bindable.TextChanged += OnTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(CustomEntry bindable)
+        {
+            bindable.TextChanged -= OnTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        #endregion
+
+        #region-- Private helpers --
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is CustomEntry entry && e.NewTextValue is not null && e.NewTextValue.Any())
+            {
+                // 1) парсит норм, виснет
+                //bool isMatch = Regex.IsMatch(e.NewTextValue, @"^([A-Za-z-,.']+\d*[ ]?)+$");
+
+                // 2) парсит норм, виснет
+                //bool isMatch = Regex.IsMatch(e.NewTextValue, @"^\b([A-Za-z-,.']+\d*[ ]*)+$");
+                _ = Task.Delay(150);
+                bool isMatch = Regex.IsMatch(e.NewTextValue, @"^\b([A-Za-z-,.']+\d*[ ]*)+$");
+
+                if (!isMatch)
+                {
+                    entry.Text = e.OldTextValue;
+                }
+                else if (e.NewTextValue.Last() != ' ')
+                {
+                    var words = e.NewTextValue
+                       .Split(' ')
+                       .Where(x => !string.IsNullOrWhiteSpace(x))
+                       .Select(x => WordToPascalCase(x));
+
+                    entry.Text = string.Join(' ', words);
+                }
+            }
+      }
+
+        private string WordToPascalCase(string word)
+        {
+            return word[0].ToString().ToUpper() + word[1..].ToLower();
+        }
+
+        #endregion
+    }
+}
