b9243cf8ea768f6fbe4683eaeca7c67e4482ac84
DESKTOP-11L4M4B\Intern_03 <satyagrah108@gmail.com>
2022-04-26 12:14:20 +0300

    add and register empty FinishPaymentDialog for Tablet and Mobile.

---
+++ Next2/src/Next2/ViewModels/Dialogs/FinishPaymentDialogViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Next2.ViewModels.Dialogs
+{
+    public class FinishPaymentDialogViewModel : BindableBase
+    {
+        public FinishPaymentDialogViewModel(Action<IDialogParameters> requestClose)
+        {
+            RequestClose = requestClose;
+            OkCommand = new Command(() => RequestClose(null));
+        }
+
+        #region -- Public properties --
+
+        public Action<IDialogParameters> RequestClose;
+
+        public ICommand OkCommand { get; }
+
+        #endregion
+    }
+}
---
+++ Next2/src/Next2/Views/Tablet/Dialogs/FinishPaymentDialog.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Next2.ViewModels.Dialogs;
+using Prism.Services.Dialogs;
+using Rg.Plugins.Popup.Pages;
+using System;
+
+namespace Next2.Views.Tablet.Dialogs
+{
+    public partial class FinishPaymentDialog : PopupPage
+    {
+        public FinishPaymentDialog(Action<IDialogParameters> requestClose)
+        {
+            InitializeComponent();
+            BindingContext = new FinishPaymentDialogViewModel(requestClose);
+        }
+    }
+}
