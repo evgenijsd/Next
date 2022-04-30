27b8266da91711a1fa8d05cbcd3ea867c408c8b3
Denys Vedernykov <razom.info@gmail.com>
2022-04-30 15:02:00 +0300

    Improved condition

--- Next2/src/Next2/ViewModels/SettingsViewModel.cs
+++ Next2/src/Next2/ViewModels/SettingsViewModel.cs
@@ -68,9 +68,7 @@ namespace Next2.ViewModels
         {
             if (dialogResult is not null)
             {
-                bool result = (bool)dialogResult?[Constants.DialogParameterKeys.ACCEPT];
-
-                if (result)
+                if (dialogResult.TryGetValue(Constants.DialogParameterKeys.ACCEPT, out bool result) && result)
                 {
                     _authenticationService.LogOut();
 
