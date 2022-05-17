54381687f26a8a5897eea44b568c40fe9cf94f53
EugenSeko <EugenSeko@users.noreply.github.com>
2022-05-17 20:24:02 +0300

    edit

--- Next2/src/Next2/Helpers/Extensions/FormatPhoneNumberExtension.cs
+++ Next2/src/Next2/Helpers/Extensions/FormatPhoneNumberExtension.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace Next2.Helpers.Extensions
 {
@@ -19,31 +16,6 @@ namespace Next2.Helpers.Extensions
 
             switch (phoneNumber.Length)
             {
-                case 5:
-                    phoneParser = new Regex(@"(\d{3})(\d{2})");
-                    format = "$1 $2";
-                    break;
-
-                case 6:
-                    phoneParser = new Regex(@"(\d{2})(\d{2})(\d{2})");
-                    format = "$1 $2 $3";
-                    break;
-
-                case 7:
-                    phoneParser = new Regex(@"(\d{3})(\d{2})(\d{2})");
-                    format = "$1 $2 $3";
-                    break;
-
-                case 8:
-                    phoneParser = new Regex(@"(\d{4})(\d{2})(\d{2})");
-                    format = "$1 $2 $3";
-                    break;
-
-                case 9:
-                    phoneParser = new Regex(@"(\d{4})(\d{3})(\d{2})(\d{2})");
-                    format = "$1 $2 $3 $4";
-                    break;
-
                 case 10:
                     phoneParser = new Regex(@"(\d{3})(\d{3})(\d{4})");
                     format = "$1-$2-$3";
@@ -51,7 +23,7 @@ namespace Next2.Helpers.Extensions
 
                 case 11:
                     phoneParser = new Regex(@"(\d{4})(\d{3})(\d{2})(\d{2})");
-                    format = "$1 $2 $3 $4";
+                    format = "$1-$2-$3-$4";
                     break;
 
                 default:
