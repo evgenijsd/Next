da536bd15c7e1d7230116e20347950f019034056
Yevhen Demchenko <yevhen.demchenko@headworks.com.ua>
2022-05-23 11:14:28 +0300

    NEX-213: - editing by comment

--- Next2/src/Next2/Models/API/DTO/SimpleSubcategoryModelDTO.cs
+++ Next2/src/Next2/Models/API/DTO/SimpleSubcategoryModelDTO.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Next2.Models.API.DTO
 {
-    public class SimpleSubCategoryDTO : IBaseApiModel
+    public class SimpleSubcategoryModelDTO : IBaseApiModel
     {
         public Guid Id { get; set; }
 
