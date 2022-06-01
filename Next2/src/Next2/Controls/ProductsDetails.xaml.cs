d97c03620bc736375f36d3a23ee1d4eb36569e82
Orel89 <oleksii.orel@headworks.com.ua>
2022-06-01 09:50:17 +0300

    Renamed ProductItemTemplate

--- Next2/src/Next2/Controls/ProductsDetails.xaml.cs
+++ Next2/src/Next2/Controls/ProductsDetails.xaml.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Next2.Controls.Templates
+namespace Next2.Controls
 {
-    public partial class ProductItemTemplate : Grid
+    public partial class ProductsDetails : Grid
     {
-        public ProductItemTemplate()
+        public ProductsDetails()
         {
             InitializeComponent();
         }
@@ -23,7 +23,7 @@ namespace Next2.Controls.Templates
             propertyName: nameof(FontSizeTitle),
             returnType: typeof(double),
             defaultValue: (double)Application.Current.Resources["TSize_i6"],
-            declaringType: typeof(ProductItemTemplate),
+            declaringType: typeof(ProductsDetails),
             defaultBindingMode: BindingMode.TwoWay);
 
         public double FontSizeTitle
@@ -35,7 +35,7 @@ namespace Next2.Controls.Templates
         public static readonly BindableProperty FontFamilyTitleProperty = BindableProperty.Create(
             propertyName: nameof(FontFamilyTitle),
             returnType: typeof(string),
-            declaringType: typeof(ProductItemTemplate),
+            declaringType: typeof(ProductsDetails),
             defaultValue: "Barlow-SemiBold",
             defaultBindingMode: BindingMode.TwoWay);
 
@@ -49,7 +49,7 @@ namespace Next2.Controls.Templates
            propertyName: nameof(FontSizePrice),
            returnType: typeof(double),
            defaultValue: (double)Application.Current.Resources["TSize_i5"],
-           declaringType: typeof(ProductItemTemplate),
+           declaringType: typeof(ProductsDetails),
            defaultBindingMode: BindingMode.TwoWay);
 
         public double FontSizePrice
@@ -61,7 +61,7 @@ namespace Next2.Controls.Templates
         public static readonly BindableProperty FontFamilyPriceProperty = BindableProperty.Create(
             propertyName: nameof(FontFamilyPrice),
             returnType: typeof(string),
-            declaringType: typeof(ProductItemTemplate),
+            declaringType: typeof(ProductsDetails),
             defaultValue: "Barlow-SemiBold",
             defaultBindingMode: BindingMode.TwoWay);
 
@@ -75,7 +75,7 @@ namespace Next2.Controls.Templates
            propertyName: nameof(FontSizeProduct),
            returnType: typeof(double),
            defaultValue: (double)Application.Current.Resources["TSize_i5"],
-           declaringType: typeof(ProductItemTemplate),
+           declaringType: typeof(ProductsDetails),
            defaultBindingMode: BindingMode.TwoWay);
 
         public double FontSizeProduct
@@ -87,7 +87,7 @@ namespace Next2.Controls.Templates
         public static readonly BindableProperty FontFamilyProductProperty = BindableProperty.Create(
             propertyName: nameof(FontFamilyProduct),
             returnType: typeof(string),
-            declaringType: typeof(ProductItemTemplate),
+            declaringType: typeof(ProductsDetails),
             defaultValue: "Barlow-Medium",
             defaultBindingMode: BindingMode.TwoWay);
 
@@ -100,7 +100,7 @@ namespace Next2.Controls.Templates
         public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create(
             nameof(ItemsSource),
             typeof(ICollection),
-            typeof(ProductItemTemplate),
+            typeof(ProductsDetails),
             default(ICollection));
 
         public ICollection ItemsSource
