0a3d28cb22b569d954cd6ab249bc329fa4d622e1
EugenSeko <EugenSeko@users.noreply.github.com>
2022-06-15 15:43:59 +0300

    fixed order update

--- Next2/src/Next2/Extensions/OrderExtension.cs
+++ Next2/src/Next2/Extensions/OrderExtension.cs
@@ -20,7 +20,7 @@ namespace Next2.Extensions
                     TotalPrice = x.TotalPrice,
                     DiscountPrice = x.DiscountPrice,
                     DishId = x.DishId,
-                    SelectedDishProportionId = x.SelectedDishProportion.Proportion.Id,
+                    SelectedDishProportionId = x.SelectedDishProportion.Id,
                     SelectedProducts = x.SelectedProducts.Select(x => new IncomingSelectedProductModel()
                     {
                         ProductId = x.Product.Id,
