9cf2de695c4bc2ff5771a79deb3cb98c66711020
Yevhen Demchenko <yevhen.demchenko@headworks.com.ua>
2022-06-17 12:28:10 +0300

    NEX-101: - heigth calculated

--- Next2/src/Next2/Views/Mobile/OrderTabsPage.xaml.cs
+++ Next2/src/Next2/Views/Mobile/OrderTabsPage.xaml.cs
@@ -17,5 +17,12 @@ namespace Next2.Views.Mobile
                 collectionView.ScrollTo(collectionView.SelectedItem, position: ScrollToPosition.Center, animate: false);
             }
         }
+
+        private void collectionView_SizeChanged(object sender, EventArgs e)
+        {
+            var x = collectionView.Height;
+            var y = view.Height;
+            int i = 0;
+        }
     }
 }
