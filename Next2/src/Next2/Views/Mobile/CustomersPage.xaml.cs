b9ab9ed48d0135a9f66cb5db4e100bc66322e26c
Yevhen Demchenko <yevhen.demchenko@headworks.com.ua>
2022-06-21 22:37:34 +0300

    NEX-101: - create behavior index last element

--- Next2/src/Next2/Views/Mobile/CustomersPage.xaml.cs
+++ Next2/src/Next2/Views/Mobile/CustomersPage.xaml.cs
@@ -4,26 +4,9 @@ namespace Next2.Views.Mobile
 {
     public partial class CustomersPage : BaseContentPage
     {
-        private double _viewItemHeight;
-
         public CustomersPage()
         {
             InitializeComponent();
-        }
-
-        public int IndexLastVisibleElement { get; set; }
-
-        private void collectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
-        {
-            if (_viewItemHeight == 0f && collectionView.ItemTemplate.CreateContent() is View view)
-            {
-                _viewItemHeight = view.HeightRequest;
-            }
-
-            if (_viewItemHeight > 0)
-            {
-                IndexLastVisibleElement = (int)((collectionView.Height + e.VerticalOffset) / _viewItemHeight);
-            }
         }
     }
 }
--- Next2/src/Next2/Views/Mobile/OrderTabsPage.xaml.cs
+++ Next2/src/Next2/Views/Mobile/OrderTabsPage.xaml.cs
@@ -5,33 +5,16 @@ namespace Next2.Views.Mobile
 {
     public partial class OrderTabsPage : BaseContentPage
     {
-        private double _viewItemHeight;
-
         public OrderTabsPage()
         {
             InitializeComponent();
         }
-
-        public int IndexLastVisibleElement { get; set; }
 
         private void collectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (collectionView?.SelectedItem != null)
             {
                 collectionView.ScrollTo(collectionView.SelectedItem, position: ScrollToPosition.Center, animate: false);
-            }
-        }
-
-        private void collectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
-        {
-            if (_viewItemHeight == 0f && collectionView.ItemTemplate.CreateContent() is View view)
-            {
-                _viewItemHeight = view.HeightRequest;
-            }
-
-            if (_viewItemHeight > 0)
-            {
-                IndexLastVisibleElement = (int)((collectionView.Height + e.VerticalOffset) / _viewItemHeight);
             }
         }
     }
