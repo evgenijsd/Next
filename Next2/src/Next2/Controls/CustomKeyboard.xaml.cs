f3335a4ee91a95167211b55d1fbbf0d4e208e0f5
EugenSeko <EugenSeko@users.noreply.github.com>
2022-06-29 20:12:29 +0300

    split the control NumericKeyboard

---
+++ Next2/src/Next2/Controls/CustomKeyboard.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Next2.Controls
+{
+    public partial class CustomKeyboard : ContentView
+    {
+        public CustomKeyboard()
+        {
+            InitializeComponent();
+        }
+
+        #region -- Public Properties
+
+        public static readonly BindableProperty BackgroundColorButtonProperty = BindableProperty.Create(
+           propertyName: nameof(BackgroundColorButton),
+           returnType: typeof(Color),
+           declaringType: typeof(CustomKeyboard),
+           defaultBindingMode: BindingMode.TwoWay);
+
+        public Color BackgroundColorButton
+        {
+            get => (Color)GetValue(BackgroundColorButtonProperty);
+            set => SetValue(BackgroundColorButtonProperty, value);
+        }
+
+        public static readonly BindableProperty ButtonTapCommandProperty = BindableProperty.Create(
+          propertyName: nameof(ButtonTapCommand),
+          returnType: typeof(ICommand),
+          declaringType: typeof(CustomKeyboard),
+          defaultBindingMode: BindingMode.TwoWay);
+
+        public ICommand ButtonTapCommand
+        {
+            get => (ICommand)GetValue(ButtonTapCommandProperty);
+            set => SetValue(ButtonTapCommandProperty, value);
+        }
+
+        public static readonly BindableProperty ButtonClearTapCommandProperty = BindableProperty.Create(
+          propertyName: nameof(ButtonClearTapCommand),
+          returnType: typeof(ICommand),
+          declaringType: typeof(CustomKeyboard),
+          defaultBindingMode: BindingMode.TwoWay);
+
+        public ICommand ButtonClearTapCommand
+        {
+            get => (ICommand)GetValue(ButtonClearTapCommandProperty);
+            set => SetValue(ButtonClearTapCommandProperty, value);
+        }
+
+        #endregion
+
+    }
+}
