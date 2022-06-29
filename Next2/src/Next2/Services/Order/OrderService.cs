f9df2773588e0cdd209a0b689512469c0a057dd0
Denys Vedernykov <razom.info@gmail.com>
2022-06-29 06:38:19 +0300

    fixed bug

--- Next2/src/Next2/Services/Order/OrderService.cs
+++ Next2/src/Next2/Services/Order/OrderService.cs
@@ -628,6 +628,8 @@ namespace Next2.Services.Order
 
                         CurrentOrder = currentOrder;
                         CurrentSeat = CurrentOrder.Seats.FirstOrDefault(x => x.Checked);
+
+                        result.SetSuccess();
                     }
                 }
             }
