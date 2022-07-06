5c4cdd40389d3f13e4f8e95801b3bc433aec6b57
DESKTOP-2KF8JSO\andrew <andrii.tantsiura@headworks.com.ua>
2022-07-06 19:17:22 +0300

    Corrections of Bonus page

--- Next2/src/Next2/Resources/Strings/Strings.Designer.cs
+++ Next2/src/Next2/Resources/Strings/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace Next2.Resources.Strings {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -1169,8 +1169,7 @@ namespace Next2.Resources.Strings {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Select a Coupon or Discount 
-        ///                 to use it..
+        ///   Looks up a localized string similar to Select a Coupon or Discount to use it..
         /// </summary>
         internal static string SelectCoupon {
             get {
