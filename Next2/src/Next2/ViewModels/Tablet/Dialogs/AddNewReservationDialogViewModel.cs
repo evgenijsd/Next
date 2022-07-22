8e90f62519b879fbfa64fd7f1ca6b35502be516b
DESKTOP-V8SE2NK\Intern_02 <razom.info@gmail.com>
2022-07-22 17:31:07 +0300

    save progress

--- Next2/src/Next2/ViewModels/Tablet/Dialogs/AddNewReservationDialogViewModel.cs
+++ Next2/src/Next2/ViewModels/Tablet/Dialogs/AddNewReservationDialogViewModel.cs
@@ -29,59 +29,35 @@ namespace Next2.ViewModels.Tablet.Dialogs
             CloseCommand = new DelegateCommand(() => RequestClose(null));
             AcceptCommand = new DelegateCommand(() => RequestClose(new DialogParameters() { { Constants.DialogParameterKeys.ACCEPT, true } }));
             DeclineCommand = new DelegateCommand(() => RequestClose(new DialogParameters() { { Constants.DialogParameterKeys.ACCEPT, false } }));
-
-            GuestsAmountList = new()
-            {
-                new()
-                {
-                    TableNumber = 55,
-                },
-                new()
-                {
-                    TableNumber = 5455,
-                },
-                new()
-                {
-                    TableNumber = 5785,
-                },
-                new()
-                {
-                    TableNumber = 1255,
-                },
-                new()
-                {
-                    TableNumber = 33355,
-                },
-            };
         }
 
         #region -- Public properties --
 
-        public ObservableCollection<TableBindableModel> GuestsAmountList { get; set; }
-
-        public bool IsExpandGuestsAmount { get; set; }
-
-        public int CurrentItem { get; set; }
+        public ObservableCollection<int> GuestsAmountList { get; set; }
 
         public string Name { get; set; } = string.Empty;
 
         public string Phone { get; set; } = string.Empty;
 
-        public string Email { get; set; } = string.Empty;
+        public int SelectedAmountGuests { get; set; }
+
+        public int SelectedTable { get; set; }
+
+        public string Notes { get; set; } = string.Empty;
 
         public DateTime? SelectedDate { get; set; } = null;
+
+        public int Hours { get; set; }
+
+        public int Minutes { get; set; }
+
+        public bool IsPMTimeFormat { get; set; }
 
         public bool IsValidName { get; set; }
 
         public bool IsValidPhone { get; set; }
 
-        public bool IsValidEmail { get; set; }
-
-        public bool CanAddNewCustomer => IsValidName && IsValidPhone && IsValidEmail && SelectedDate is not null;
-
-        public bool IsEntriesEnabled { get; set; } = true;
-
-        public EClientAdditionStep Step { get; set; } = EClientAdditionStep.Info;
+        public bool CanAddNewReservation => IsValidName && IsValidPhone && SelectedTable > 0 && SelectedDate is not null;
 
         public Action<IDialogParameters> RequestClose;
 
@@ -91,74 +67,34 @@ namespace Next2.ViewModels.Tablet.Dialogs
 
         public DelegateCommand DeclineCommand { get; }
 
-        private ICommand _goToStepCommand;
-        public ICommand GoToStepCommand => _goToStepCommand ??= new AsyncCommand<EClientAdditionStep>(OnGoToStepCommandAsync, allowsMultipleExecutions: false);
-
-        private ICommand _addNewCustomerCommand;
-        public ICommand AddNewCustomerCommand => _addNewCustomerCommand ??= new AsyncCommand(OnAddNewCustomerCommandAsync, allowsMultipleExecutions: false);
-
-        private ICommand _expandGuestsAmountCommand;
-        public ICommand ExpandGuestsAmountCommand => _expandGuestsAmountCommand ??= new AsyncCommand(OnExpandGuestsAmountCommandAsync, allowsMultipleExecutions: false);
+        private ICommand _addNewReservationCommand;
+        public ICommand AddNewReservationCommand => _addNewReservationCommand ??= new AsyncCommand(OnAddNewReservationCommandAsync, allowsMultipleExecutions: false);
 
         #endregion
 
         #region -- Overrides --
 
-        private Task OnExpandGuestsAmountCommandAsync()
-        {
-            IsExpandGuestsAmount = true;
-
-            return Task.CompletedTask;
-        }
-
         protected override void OnPropertyChanged(PropertyChangedEventArgs args)
         {
             base.OnPropertyChanged(args);
-
-            if (args.PropertyName is nameof(Step))
-            {
-                IsEntriesEnabled = Step == EClientAdditionStep.Info;
-            }
         }
 
         #endregion
 
         #region -- Private helpers --
 
-        private async Task OnAddNewCustomerCommandAsync()
+        private async Task OnAddNewReservationCommandAsync()
         {
-            if (CanAddNewCustomer)
-            {
-                var newCustomer = new CustomerBindableModel()
-                {
-                    Email = Email,
-                    FullName = Name,
-                    Phone = Phone,
-                    Birthday = SelectedDate,
-                };
-
-                var result = await _customersService.CreateCustomerAsync(newCustomer);
-
-                if (result.IsSuccess)
-                {
-                    var parameters = new DialogParameters()
-                    {
-                        { Constants.DialogParameterKeys.ACCEPT, true },
-                        { Constants.DialogParameterKeys.CUSTOMER_ID, result.Result },
-                    };
-
-                    AcceptCommand = new DelegateCommand(() => RequestClose(parameters));
-                }
-
-                AcceptCommand.Execute();
-            }
         }
 
-        private Task OnGoToStepCommandAsync(EClientAdditionStep step)
+        private void GenerateGuestsAmountList()
         {
-            Step = step;
+            GuestsAmountList = new();
 
-            return Task.CompletedTask;
+            for (int i = 0; i < 25; i++)
+            {
+                GuestsAmountList.Add(i);
+            }
         }
 
         #endregion
