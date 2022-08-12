1a4d94348033fcf2870ba43b4ede5eb7ed3f69c4
EugenSeko <EugenSeko@users.noreply.github.com>
2022-08-12 22:55:56 +0300

    edit

--- Next2/src/Next2/Models/API/DTO/DishReplacementProductModelDTO.cs
+++ Next2/src/Next2/Models/API/DTO/DishReplacementProductModelDTO.cs
@@ -8,7 +8,9 @@ namespace Next2.Models.API.DTO
     public class DishReplacementProductModelDTO : IBaseApiModel
     {
         public Guid Id { get; set; }
+
         public Guid ProductId { get; set; }
+
         public IEnumerable<SimpleProductModelDTO>? Products { get; set; }
     }
 }
