0d3db369792349965bdf064a72e9180f2c32bf37
DESKTOP-11L4M4B\Intern_03 <satyagrah108@gmail.com>
2022-08-12 15:19:02 +0300

    save

---
+++ Next2/src/Next2/ViewModels/Dialogs/ProgrammDeviceDialogViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using System;
+
+namespace Next2.ViewModels.Dialogs
+{
+    public class ProgrammDeviceDialogViewModel : BindableBase
+    {
+        public ProgrammDeviceDialogViewModel(
+            DialogParameters param,
+            Action<IDialogParameters> requestClose)
+        {
+            RequestClose = requestClose;
+
+            InitParameters(param);
+        }
+
+        #region -- Public Properties --
+
+        public Action<IDialogParameters>? RequestClose;
+
+        #endregion
+
+        #region -- Private Helpers --
+
+        private void InitParameters(IDialogParameters param)
+        {
+        }
+
+        #endregion
+    }
+}
