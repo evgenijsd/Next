75ed9a4301f8a585bf7cdea13dcec1124a78d69a
evgenijsd <yevhen.demchenko@headworks.com.ua>
2022-09-05 22:00:22 +0300

    editing

--- Next2/src/Next2/Controls/StepperNumber.xaml.cs
+++ Next2/src/Next2/Controls/StepperNumber.xaml.cs
@@ -33,16 +33,16 @@ namespace Next2.Controls
             set => SetValue(FontSizeProperty, value);
         }
 
-        public static readonly BindableProperty NumberProperty = BindableProperty.Create(
-            propertyName: nameof(Number),
+        public static readonly BindableProperty ValueProperty = BindableProperty.Create(
+            propertyName: nameof(Value),
             returnType: typeof(int),
             declaringType: typeof(StepperNumber),
             defaultBindingMode: BindingMode.TwoWay);
 
-        public int Number
+        public int Value
         {
-            get => (int)GetValue(NumberProperty);
-            set => SetValue(NumberProperty, value);
+            get => (int)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
         }
 
         public static readonly BindableProperty MinNumberProperty = BindableProperty.Create(
@@ -104,7 +104,7 @@ namespace Next2.Controls
 
         private Task OnChangeNumberCommandAsync(ENumberChange? numberChange)
         {
-            int number = Number;
+            int number = Value;
 
             switch (numberChange ?? ENumberChange.None)
             {
@@ -124,7 +124,7 @@ namespace Next2.Controls
                     break;
             }
 
-            Number = number;
+            Value = number;
 
             NumberChangedAction();
 
